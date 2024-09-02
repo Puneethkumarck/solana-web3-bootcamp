3c0079bed63679db28f3fc4325173c587c3df967
PuneethKumar Ck <punith.564@gmail.com>
2024-09-03 00:42:14 +0200

    remove all

--- generate-keypair.js
+++
@@ -1,4 +0,0 @@
-import { Keypair } from "@solana/web3.js";
-const keyPair = Keypair.generate();
-console.log('public key is', keyPair.publicKey.toBase58())
-console.log('keypair generated' , keyPair.secretKey)
